9436842d8dc6201db82b0f8655077912019e4d41
lnxsnw <113366745+lnxsnw@users.noreply.github.com>
2025-01-12 19:26:17 +0800

    Added Config Options

--- cf/template/template.js
+++ cf/template/template.js
@@ -1,3 +1,9 @@
+const config = {
+  // This should be at the very top to edit easily
+  // but remove when not in use.
+  // To use: config.VARIABLE_HERE
+};
+
 // Hear me out, this format somehow fixes the headaches of workers code not working "sometimes".
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request));
